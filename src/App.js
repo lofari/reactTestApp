a1bb93ca9fd5570df37af7c1fb02454a9132dc56
Lor <lorenzo@amalgama.co>
2019-08-06 12:46:05 -0300

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Tweet from './Tweet'
+function App(){
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const increment = () => {
+    setCount(count+1)
+  };
+  return(
+    <div className="app">
+   
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+
+     <Tweet name="Jon snow" message="Cuanto más le das a un rey, más quiere. Estamos caminando sobre un puente de hielo con un abismo a cada lado… Agradecer a un rey es bastante difícil, complacer a dos es casi imposible"/> 
+     <Tweet name="Yoda" message="La muerte una parte natural de la vida es. Regocíjate por los que te rodean que en la Fuerza se transforman. Llorarlos no debes. Añorarlos tampoco. El apego a los celos conduce. La negra sombra de la codicia es" />
+     <Tweet name="Perón" message="Ningún Peronista debe sentirse más de lo que es, ni menos de lo que debe ser. Cuando un Peronista comienza a sentirse más de lo que es, empieza a convertirse en oligarca."/>
+     <Tweet name="Scooby doo" message="Scooby-Dooby the fucking Doo"/>
     </div>
   );
 }
 
-export default App;
+export default App; 
